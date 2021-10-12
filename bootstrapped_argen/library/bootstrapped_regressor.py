6fe9fb3378a7531d8239d6d9194953128ef457be
yujiading <yujia.ding@cgu.edu>
2021-10-12 16:37:11 -0700

    Pickle bootstrapped subsets for index tracking. Refactor bootstrapped_regressor.py and driver_index_tracking.py.

--- bootstrapped_argen/library/bootstrapped_regressor.py
+++ bootstrapped_argen/library/bootstrapped_regressor.py
@@ -73,7 +73,7 @@ class BootstrappedRegressor:
                 with Pool(self.cpu) as p:
                     is_tqdm = False
                     if is_tqdm:
-                        J_lst = list(tqdm(p.imap(partial(self.get_J,
+                        J_lst = list(tqdm(p.map(partial(self.get_J,
                                                          X=X,
                                                          y=y,
                                                          is_bootstrap=True), bootstrap_iterate),
@@ -114,7 +114,7 @@ class BootstrappedRegressor:
                     self.intercept_ = 0
                     self.coef_ = reg_ls.coef_
             else:
-                reg_ls = regressor.fit(X_J, y)
+                reg_ls = regressor.bootstrapped_feature_select_hyperparameters(X_J, y)
                 self.intercept_ = reg_ls.intercept_
                 self.coef_ = reg_ls.coef_
         return self
@@ -137,121 +137,3 @@ class BootstrappedRegressor:
         y_pred = self.predict(X=X)
         mse = mean_squared_error(y, y_pred)
         return mse
-
-# class BootstrappedRegressor:
-#     def __init__(self, regressor, bootstrap_replicates: int, cpu: int = 3, aren_fit_intercept: bool = True):
-#         self.regressor = regressor
-#         self.bootstrap_replicates = bootstrap_replicates
-#         self.cpu = cpu
-#         self.aren_fit_intercept = aren_fit_intercept
-#         self.intercept_ = None
-#         self.coef_ = None
-#         self.J = None
-#
-#     @staticmethod
-#     def get_nonzero_index(lst: List, zero_thresh=1.01e-8):
-#         index = [i for i, ele in enumerate(lst) if abs(ele) > zero_thresh]
-#         return index
-#
-#     @staticmethod
-#     def get_intersect_two_lists(lst_one: List, lst_two: List):
-#         if lst_one and lst_two:
-#             return list(set(lst_one) & set(lst_two))
-#         else:
-#             if lst_one:
-#                 return lst_one
-#             else:
-#                 return lst_two
-#
-#     @staticmethod
-#     def get_intersect_lists(lsts: List[List]):
-#         ret = []
-#         for lst in lsts:
-#             ret = BootstrappedRegressor.get_intersect_two_lists(lst_one=ret, lst_two=lst)
-#         return ret
-#
-#     def get_J(self, i, X, y, is_bootstrap):
-#         n = X.shape[0]
-#         if is_bootstrap:
-#             X_star, y_star = resample(X, y, replace=True, n_samples=n)
-#         else:
-#             X_star = X
-#             y_star = y
-#         if isinstance(self.regressor, GeneralizedElasticNet) and self.aren_fit_intercept:
-#             X_star = np.concatenate((np.ones((n, 1)), X_star), axis=1)
-#         reg = self.regressor.fit(X_star, y_star)
-#         if isinstance(self.regressor, GeneralizedElasticNet) and self.aren_fit_intercept:
-#             w = reg.coef_[1:]
-#         else:
-#             w = reg.coef_
-#         J_ = self.get_nonzero_index(lst=w)
-#         return J_
-#
-#     def fit_subset(self, X, y):
-#         if self.bootstrap_replicates is None:
-#             J = self.get_J(X=X, y=y, is_bootstrap=False, i=1)
-#         else:
-#             parallel = True
-#             if parallel:
-#                 bootstrap_iterate = list(range(self.bootstrap_replicates))
-#                 with Pool(self.cpu) as p:
-#                     is_tqdm = False
-#                     if is_tqdm:
-#                         J_lst = list(tqdm(p.imap(partial(self.get_J,
-#                                                          X=X,
-#                                                          y=y,
-#                                                          is_bootstrap=True), bootstrap_iterate),
-#                                           total=len(bootstrap_iterate)))
-#                     else:
-#                         J_lst = list(p.map(partial(self.get_J,
-#                                                    X=X,
-#                                                    y=y,
-#                                                    is_bootstrap=True), bootstrap_iterate))
-#
-#                 J = self.get_intersect_lists(J_lst)
-#             else:
-#                 J = []
-#                 for i in tqdm(range(self.bootstrap_replicates)):
-#                     J_ = self.get_J(X=X, y=y, is_bootstrap=True, i=1)
-#                     J = self.get_intersect_two_lists(lst_one=J, lst_two=J_)
-#         self.J = J
-#         return self
-#
-#     def fit_regression(self, X, y, regressor=None, fit_intercept: bool = True):
-#         X_J = X.iloc[:, self.J]
-#         if regressor is None:
-#             reg_ls = LinearRegression(fit_intercept=fit_intercept).fit(X_J, y)
-#             self.intercept_ = reg_ls.intercept_
-#             self.coef_ = reg_ls.coef_
-#         else:
-#             if isinstance(regressor, GeneralizedElasticNet):
-#                 if fit_intercept:
-#                     n = X_J.shape[0]
-#                     X_J = np.concatenate((np.ones((n, 1)), X_J), axis=1)
-#                     reg_ls = regressor.fit(X_J, y)
-#                     self.intercept_ = reg_ls.coef_[0]
-#                     self.coef_ = reg_ls.coef_[1:]
-#                 else:
-#                     reg_ls = regressor.fit(X_J, y)
-#                     self.intercept_ = 0
-#                     self.coef_ = reg_ls.coef_
-#             else:
-#                 reg_ls = regressor.fit(X_J, y)
-#                 self.intercept_ = reg_ls.intercept_
-#                 self.coef_ = reg_ls.coef_
-#         return self
-#
-#     def predict(self, X):
-#         """
-#         X has same columns as X in fit
-#         """
-#         X_J = X.iloc[:, self.J]
-#         return np.matmul(X_J, self.coef_) + self.intercept_
-#
-#     def score(self, X, y):
-#         """
-#         X has same columns as X in fit
-#         """
-#         y_pred = self.predict(X=X)
-#         mse = mean_squared_error(y, y_pred)
-#         return mse
--- tests/test_driver_index_tracking.py
+++ tests/test_driver_index_tracking.py
@@ -2,16 +2,14 @@ from bootstrapped_argen.driver_index_tracking import DriverIndexTrackSp500Aren, 
 
 
 def test_driver_index_track_sp500_aren():
-    driver = DriverIndexTrackSp500Aren(bootstrap_replicates=None,
+    driver = DriverIndexTrackSp500Aren(bootstrap_replicates=2,
                                        n_alphas=2,  # from 0 to 1
-                                       n_lambdas=2,  # from e^-1 to e^5
-                                       start_date='2018-09-01',
+                                       n_lambdas=2,  # from e^1 to e^15
+                                       start_date='2020-09-01',
                                        end_date='2021-09-01',
                                        train_size=0.7,
                                        val_size=0.2,
-                                       test_size=0.1,
-                                       lower_bound=0.0058,
-                                       upper_bound=0.1)
+                                       test_size=0.1)
     mse, portfolio_return, cumulative_return, annual_average_return, \
     annual_volatility, daily_tracking_error, daily_tracking_error_volatility = driver.run
     print(f"portfolio_return is {portfolio_return}")
