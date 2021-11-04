6521cc14b82486141a62189f5546d092b73df8c7
yujiading <yujia.ding@cgu.edu>
2021-11-04 15:16:35 -0700

    Add soft version of boaren; Use return instead of price for regression; Rework lam and alpha list using max lam suggested by lasso paper; Add plot for predicted price;

--- bootstrapped_argen/library/bootstrapped_regressor.py
+++ bootstrapped_argen/library/bootstrapped_regressor.py
@@ -16,36 +16,51 @@ class BootstrappedRegressor:
                  bootstrap_replicates: int,
                  bootstrapped_feature_select_regressor,
                  argen_fit_intercept: bool = True,
-                 cpu: int = 8):
+                 cpu: int = 8,
+                 zero_thresh=1e-8,
+                 is_soft_J: bool = True):
         self.bootstrap_replicates = bootstrap_replicates
         self.bootstrapped_feature_select_regressor = bootstrapped_feature_select_regressor
         self.argen_fit_intercept = argen_fit_intercept
         self.cpu = cpu
+        self.zero_thresh = zero_thresh
+        self.is_soft_J = is_soft_J
         self.intercept_ = None
         self.coef_ = None
         self.J = None
+        self.J_soft = None
 
-    @staticmethod
-    def _get_nonzero_index(lst: List, zero_thresh=1.01e-8):
-        index = [i for i, ele in enumerate(lst) if abs(ele) > zero_thresh]
+    def _get_nonzero_index(self, lst: List):
+        index = [i for i, ele in enumerate(lst) if abs(ele) > self.zero_thresh]
         return index
 
+    # @staticmethod
+    # def _get_intersect_two_lists(lst_one: List, lst_two: List):
+    #     if lst_one and lst_two:
+    #         return list(set(lst_one) & set(lst_two))
+    #     else:
+    #         if lst_one:
+    #             return lst_one
+    #         else:
+    #             return lst_two
+    #
+    # @staticmethod
+    # def _get_intersect_lists(lsts: List[List]):
+    #     ret = []
+    #     for lst in lsts:
+    #         ret = BootstrappedRegressor._get_intersect_two_lists(lst_one=ret, lst_two=lst)
+    #     return ret
     @staticmethod
-    def _get_intersect_two_lists(lst_one: List, lst_two: List):
-        if lst_one and lst_two:
-            return list(set(lst_one) & set(lst_two))
-        else:
-            if lst_one:
-                return lst_one
-            else:
-                return lst_two
-
-    @staticmethod
-    def _get_intersect_lists(lsts: List[List]):
-        ret = []
+    def _get_intersect_lists(lsts: List[List], percentage: float):
+        length = len(lsts)
+        count_dict = {}
         for lst in lsts:
-            ret = BootstrappedRegressor._get_intersect_two_lists(lst_one=ret, lst_two=lst)
-        return ret
+            for item in lst:
+                if item in count_dict:
+                    count_dict[item] += 1
+                else:
+                    count_dict[item] = 1
+        return [item for item in count_dict if count_dict[item] / length >= percentage]
 
     def get_J(self, i, X, y, is_bootstrap):
         n = X.shape[0]
@@ -67,7 +82,7 @@ class BootstrappedRegressor:
 
     def bootstrapped_feature_select(self, X, y):
         if self.bootstrap_replicates is None:
-            J = self.get_J(X=X, y=y, is_bootstrap=False, i=1)
+            self.J = self.get_J(X=X, y=y, is_bootstrap=False, i=1)
         else:
             parallel = True
             if parallel:
@@ -82,23 +97,28 @@ class BootstrappedRegressor:
                                           total=len(bootstrap_iterate)))
                     else:
                         J_lst = list(p.imap(partial(self.get_J,
-                                                   X=X,
-                                                   y=y,
-                                                   is_bootstrap=True), bootstrap_iterate, chunksize=2))
+                                                    X=X,
+                                                    y=y,
+                                                    is_bootstrap=True), bootstrap_iterate, chunksize=2))
 
-                J = self._get_intersect_lists(J_lst)
             else:
-                J = []
+                J_lst = []
                 for i in tqdm(range(self.bootstrap_replicates)):
                     J_ = self.get_J(X=X, y=y, is_bootstrap=True, i=1)
-                    J = self._get_intersect_two_lists(lst_one=J, lst_two=J_)
-        self.J = J
+                    J_lst.append(J_)
+            self.J = self._get_intersect_lists(J_lst, percentage=1)
+            if self.is_soft_J:
+                self.J_soft = self._get_intersect_lists(J_lst, percentage=0.9)
         return self
 
-    def fit(self, X, y, regressor=None, fit_intercept: bool = True):
+    def fit(self, X, y, is_soft_J, regressor=None, fit_intercept: bool = True):
         if self.J is None:
             raise ValueError('Need to run bootstrapped_feature_select')
-        X_J = X.iloc[:, self.J]
+        if is_soft_J:
+            J = self.J_soft
+        else:
+            J = self.J
+        X_J = X.iloc[:, J]
         if regressor is None:
             reg_ls = LinearRegression(fit_intercept=fit_intercept).fit(X_J, y)
             self.intercept_ = reg_ls.intercept_
@@ -121,7 +141,7 @@ class BootstrappedRegressor:
                 self.coef_ = reg_ls.coef_
         return self
 
-    def predict(self, X):
+    def predict(self, X, is_soft_J):
         """
         X has same columns as X in fit
         """
@@ -129,13 +149,17 @@ class BootstrappedRegressor:
             raise ValueError('Need to run bootstrapped_feature_select')
         if self.coef_ is None or self.intercept_ is None:
             raise ValueError('Need to run fit')
-        X_J = X.iloc[:, self.J]
+        if is_soft_J:
+            J = self.J_soft
+        else:
+            J = self.J
+        X_J = X.iloc[:, J]
         return np.matmul(X_J, self.coef_) + self.intercept_
 
-    def score(self, X, y):
+    def score(self, X, y, is_soft_J):
         """
         X has same columns as X in fit
         """
-        y_pred = self.predict(X=X)
+        y_pred = self.predict(X=X, is_soft_J=is_soft_J)
         mse = mean_squared_error(y, y_pred)
         return mse
